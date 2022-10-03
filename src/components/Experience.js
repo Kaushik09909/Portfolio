3aea2667322b91874f2f269d58c5e1f21278e8bd
kaushik09909 <kaushik09909@gmail.com>
2022-10-03 23:58:37 +0530

    Fixed date calculation issue

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -7,7 +7,7 @@ import { Media } from "reactstrap";
 function getDuration(duration) {
   const years = parseInt(duration / 12);
   const months = (duration > 12)? duration % 12 : duration
-  return (years > 0? years + " year" + (years > 1? "s" + (months === 0 ? "" : " and ") : "") : "") + (months > 0? months + " month" + (months > 1? "s": "") : "");
+  return (years > 0? years + " year" + (years > 1? "s" + (months === 0 ? "" : " and ") : "") : "") + ((months > 0 && months % 12)? months + " month" + (months > 1? "s": "") : "");
 };
 
 class Experience extends React.Component {
